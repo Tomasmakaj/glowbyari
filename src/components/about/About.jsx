4c661bd097e51768acdcb5f0a3e18986504cfa4f
Tomas Makaj <Tmakajcoding@gmail.com>
2022-12-01 09:55:02 -0500

    about ari

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,10 +7,19 @@ import glowabout from '../../assets/glowabout.svg'
 const About = () => {
   return (
     <div className='about'>
-        <div>
+        <div className='aboutAri'>
         <img src={glowabout} className='aboutLogo' />
+          <p>My name is Arjana and everyone calls me Ari. 
+            I love skin care and love to help people feel better under their skin. 
+            I went to New York School of Esthetics where I studied and got my license with the state of New York. 
+            I am an Albanian who was born in Kosovo, migrated to America for a better life and more opportunity. 
+            Being an esthetician is something I’ve been wanting to do and take great pride in. 
+            I want to help my clients have better knowledge of products and ingredients that are more affective for their skin type. 
+            As well as feeling comfortable and happy with what their skin looks like. 
+            I have a favorite quote that I like to use 
+            “Taking care of your skin, is more important than covering it up”.  
+            </p>
         </div>
-        <p></p>
     </div>
   )
 }
